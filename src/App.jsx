194949eb1c2b40f2b9dec9d159256a3200a46de2
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-07-20 10:49:19 +0300

    Create and render Header component

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-    return (
-        <div>
-            <h2>Here I am</h2>
-        </div>
-    );
-}
-export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <div className="header">
+            <h1>SMART GOAL PLANNER</h1>
+        </div>
+    );
+}
+export default Header;
